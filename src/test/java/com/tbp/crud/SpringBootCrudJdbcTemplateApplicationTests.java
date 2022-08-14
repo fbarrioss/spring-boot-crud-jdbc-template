a4f3be3ca691f03a337f8dc4120f636b251fb672
Franklin Barrios <39170307+fbarrioss@users.noreply.github.com>
2022-08-13 23:38:27 -0500

    agregando test unitarios

--- src/test/java/com/tbp/crud/SpringBootCrudJdbcTemplateApplicationTests.java
+++ src/test/java/com/tbp/crud/SpringBootCrudJdbcTemplateApplicationTests.java
@@ -1,13 +1,30 @@
 package com.tbp.crud;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 
-@SpringBootTest
-class SpringBootCrudJdbcTemplateApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
+public class SpringBootCrudJdbcTemplateApplicationTests {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory ();
+		return new RestTemplate(factory);
+	}
 
 	@Test
-	void contextLoads() {
+	public void consultarUsuarioTest() {
+		String body = this.restTemplate().getForObject("http://localhost:8080/user/1", String.class);
+		assertThat(body,containsString("Barrios"));
 	}
 
 }
